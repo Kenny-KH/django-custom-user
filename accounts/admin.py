521d3925aa31b98228d5864348c37135cdec4b83
Kenny-KH <77623474+Kenny-KH@users.noreply.github.com>
2022-08-10 16:59:43 +0900

    Initial commit

---
+++ accounts/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .form import UserCreationForm, UserChangeForm
+from .models import User
+
+# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    
+    list_display = ('name', 'email', 'address', 'phone_number', 'is_admin')
+    list_filter = ('is_admin' , )
+    fieldsets = (
+        (None,{'fields':('name','email', 'password')}),
+        ("Personal info", {'fields':('address','phone_number')}),
+        ("Permissions",{'fields':('is_admin')})
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields' : ('name','email', 'address','phone_number','password1','password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email' ,)
+    filter_horizontal = ()
+    
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
