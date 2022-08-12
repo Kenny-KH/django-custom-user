f19fce4310bd442cdb74d01c2d796baceb2dddb8
Kenny-KH <77623474+Kenny-KH@users.noreply.github.com>
2022-08-12 18:09:36 +0900

    [FIX] forms 수정

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .form import UserCreationForm, UserChangeForm
+from .forms import UserCreationForm, UserChangeForm
 from .models import User
 
 # Register your models here.
